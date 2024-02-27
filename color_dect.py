6b090aef51b520bc41c1b0a60603bd055082f967
tyunh-lab <tyunhs@gmail.com>
2024-02-27 23:12:32 +0900

    動画の読み込みを追加し、オレンジ色のピクセルを抽出する処理を修正

--- color_dect.py
+++ color_dect.py
@@ -1,39 +1,36 @@
 import cv2 
-import requests
 import numpy as np
 
 
 # カメラの読込み
 # 内蔵カメラがある場合、下記引数の数字を変更する必要あり
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
+# 動画の読込み
+cap = cv2.VideoCapture("IMG_5908.mp4")
 
-while(cap.isOpened()):
+while (cap.isOpened()):
     # 1フレーム毎　読込み
     ret, frame = cap.read()
+    # フレームがない場合、終了
+    if (frame is None):
+        break
+    # 画像をHSVに変換
+    hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-# url = "http://192.168.1.162/capture"
+    # オレンジ色のHSV範囲を指定
+    # lower_orange = np.array([30, 80, 80])  # 下限値
+    # upper_orange = np.array([45, 255, 255])  # 上限値
+    lower_orange = np.array([0, 0, 0])  # 下限値
+    upper_orange = np.array([255, 255, 255])  # 上限値
 
+    # 指定した範囲内のピクセルを抽出
+    orange_mask = cv2.inRange(hsv_image, lower_orange, upper_orange)
 
-# while True:
-    # response = requests.get(url)
-    # frame = cv2.imdecode(np.asarray(bytearray(response.content), dtype=np.uint8), 1)
-    # # HSVに変換
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #画像のサイズ
-    # frame.shape[0] 縦
-    # frame.shape[1] 横
-
-    # オレンジのHSV範囲
-    lower_orange = np.array([3, 100, 100])
-    upper_orange = np.array([10, 255, 255])
-
-    # 白以外にマスク
-    mask_white = cv2.inRange(hsv, lower_orange, upper_orange)
-    res_white = cv2.bitwise_and(frame,frame, mask= mask_white)
+    res_orange = cv2.bitwise_and(frame,frame, mask= orange_mask)
 
     # 輪郭抽出
-    gray = cv2.cvtColor(res_white, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(res_orange, cv2.COLOR_RGB2GRAY)
     ret, thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -45,6 +42,10 @@ while(cap.isOpened()):
         center = (int(x),int(y))
         # ボールの座標
         # print("x: ", f"{x:.2f}", "y: ", f"{y:.2f}", "radius: ", f"{radius:.2f}",)
+
+        # 円の中のは省く
+        if cv2.pointPolygonTest(contours[i], center, True) > -1:
+            continue
         
         # 中心からのx,yの距離
         print("x: ", f"{(x - frame.shape[1]/2):.2f}", "y: ", f"{(y - frame.shape[0]/2):.2f}", "radius: ", f"{radius:.2f}",)
@@ -58,7 +59,6 @@ while(cap.isOpened()):
         # 角度を表示
         angle = np.arctan2(y - frame.shape[0]/2, x - frame.shape[1]/2) * 180 / np.pi
         frame = cv2.putText(frame, f"{angle:.2f}", (int(x),int(y)), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 2, cv2.LINE_4)
-
         #大きさを表示
         frame = cv2.putText(frame, f"{radius:.2f}", (int(x),int(y) + 20), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 2, cv2.LINE_4)
 
@@ -76,35 +76,5 @@ while(cap.isOpened()):
         break
 
 # 終了処理
-# cap.release()
+cap.release()
 cv2.destroyAllWindows()
-
-
-# import cv2
-
-
-# img = cv2.imread('./input.jpg')
-# output_img = img.copy()
-
-# img = img[0:540, 0:1920]
-
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS_FULL)
-
-# mask_red1 = cv2.inRange(img, (0, 127, 127), (30, 255, 255))
-# mask_red2 = cv2.inRange(img, (240, 127, 127), (255, 255, 255))
-# img = cv2.bitwise_or(mask_red1, mask_red2)
-
-# circles = cv2.HoughCircles(image=img,
-#                            method=cv2.HOUGH_GRADIENT,
-#                            dp=1.0,
-#                            minDist=100,
-#                            param1=100,
-#                            param2=10,
-#                            minRadius=0,
-#                            maxRadius=50)
-
-
-# for center_x, center_y, radius in circles.squeeze(axis=0).astype(int):
-#    cv2.circle(output_img, (center_x, center_y), radius+40, (0, 0, 255), 10)
-
-# cv2.imwrite('./output.jpg', output_img)
