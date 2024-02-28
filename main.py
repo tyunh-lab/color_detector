113cd087e05b665c06d315d414d0f74286742396
tyunh-lab <tyunhs@gmail.com>
2024-02-28 15:14:56 +0900

    計測を追加

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import time
 import cv2
+
 from ball_ditect import color_ditect
 from goal_ditect import blue_goal_ditect, yellow_goal_ditect
 
 def main():
+    start = time.time()
     cap = cv2.VideoCapture("video/ball.mp4")
     color_ditect(cap)
+    print("time: ", time.time() - start)
     cap = cv2.VideoCapture("video/blue_goal.mp4")
     blue_goal_ditect(cap)
+    print("time: ", time.time() - start)
     cap = cv2.VideoCapture("video/yellow_goal.mp4")
     yellow_goal_ditect(cap)
+    print("time: ", time.time() - start)
+
 
 if __name__ == "__main__":
     main()
