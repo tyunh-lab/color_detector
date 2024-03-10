8f045168c636e19d1c4f626af0eb5ed555eca5b8
tyunh-lab <tyunhs@gmail.com>
2024-03-10 21:39:27 +0900

    firebaseにpushする機能の追加

--- util/uart.py
+++ util/uart.py
@@ -1,9 +1,13 @@
 import datetime
 import serial
 
+from firebase_manager import pushData
+
 ser = serial.Serial('/dev/ttyS0', 115200, timeout=2)
 while True:
+    ser.write(b'hello\n')
     r = ser.readline()
+    r = r.decode('utf-8')
     # ser.write(b'hello\n')
     angle = r.split(',')[0]
     motor0 = r.split(',')[1]
@@ -11,3 +15,11 @@ while True:
     motor2 = r.split(',')[3]
     motor3 = r.split(',')[4]
     print(f'angle: {angle}, motor0: {motor0}, motor1: {motor1}, motor2: {motor2}, motor3: {motor3}')
+    pushData({
+        u'angle': angle,
+        u'motor0': motor0,
+        u'motor1': motor1,
+        u'motor2': motor2,
+        u'motor3': motor3,
+        u'time': datetime.datetime.now()
+    })
