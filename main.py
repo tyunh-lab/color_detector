ecd898f814b88531694926b4f1dd9ae12d5953b1
四津谷　舜 <tyunhs@gmail.com>
2024-03-26 21:44:10 +0900

    動画の保存

--- main.py
+++ main.py
@@ -2,25 +2,21 @@ import cv2
 import threading
 
 from ditector.all_ditector import all_ditect
-
-# from util.uart import uart
-
+from util.uart import uart
 from util.json_manager import setup_json, write_json_overwrite
-
 import logger
 
-dir_path = "video"
-
 def main():
-    all_ditect()
+    all_ditect(withSaveVideo=True)
     write_json_overwrite({'status': 'done', 'now_task': 'none'})
 
 if __name__ == "__main__":
     setup_json()
+    uart()
     thread1 = threading.Thread(target=logger.logger)
-    # thread2 = threading.Thread(target=uart)
+    thread2 = threading.Thread(target=uart)
     thread1.start()
-    # thread2.start()
+    thread2.start()
     # thread1.join() #<-終わるまで次に進まない
     main()
 
